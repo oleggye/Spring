fe2500339a709f01f73a887da2fa120e7d1677a2
oleggye <4ydak2008@gmail.com>
2017-07-20 16:26:45 +0300

    edited

--- app/src/main/java/by/epam/spring/mvc/controller/CalcController.java
+++ app/src/main/java/by/epam/spring/mvc/controller/CalcController.java
@@ -1,17 +1,56 @@
 package by.epam.spring.mvc.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/calculate")
 public class CalcController {
 
-	@RequestMapping("/calculate")
-	public ModelAndView calculate(HttpServletRequest request) {
-		System.out.println("calculate");
-		return null;
+	@GetMapping
+	public ModelAndView calculate(@RequestParam(value = "first", required = true) int first,
+			@RequestParam("second") int second) {
+		System.out.println("CALCULATE EXECUTED!");
+
+		ModelAndView modelAndView = new ModelAndView("displaySum");
+		modelAndView.addObject("sum", Integer.toString(first + second));
+
+		return modelAndView;
 	}
+
+	/*
+	 * @ExceptionHandler(Exception.class)
+	 * 
+	 * @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) private ModelAndView
+	 * onException(Exception exception) {
+	 * 
+	 * 
+	 * }
+	 */
+
+	/*
+	 * @RequestMapping("/calculate") public ModelAndView
+	 * calculate(HttpServletRequest request) { System.out.println("calculate");
+	 * 
+	 * String first = request.getParameter("first"); String second =
+	 * request.getParameter("second");
+	 * 
+	 * int sum = 0;
+	 * 
+	 * ModelAndView modelAndView = new ModelAndView("displaySum");
+	 * 
+	 * if (first != null && second != null) { int firstInt =
+	 * Integer.parseInt(first); int secondInt = Integer.parseInt(second); sum =
+	 * firstInt + secondInt; }
+	 * 
+	 * modelAndView.addObject("sum", Integer.toString(sum));
+	 * 
+	 * return modelAndView; }
+	 */
 }
---
+++ app/src/main/webapp/WEB-INF/jsp/displaySum.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; utf-8"
+	pageEncoding="utf-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>The result of calculating: ${sum}</h1>
+
+</body>
+</html>
